4230750d7ffc2df0e9c211452ba6c47e67ed7c7e
Adam Budai <budai8adam@gmail.com>
2018-01-10 10:43:09 +0100

    Weight calculator was updated.

--- livepoly.py
+++ livepoly.py
@@ -396,7 +396,7 @@ elif args.mode == 2:
     # Calculate the weights
     model = nn.load_model(args.model_name, args.memory)
 
-    for idx in range(0, len(test_img_paths), 2):
+    for idx in range(0, len(test_img_paths), 3):
 
         path = test_img_paths[idx]
         weight_file = path.replace(".png", "_w.json")
