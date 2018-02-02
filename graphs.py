ba95bf6419aa920d241f0d159abd411a65eb01d8
Adam Budai <budai8adam@gmail.com>
2018-02-02 18:39:31 +0100

    Heat map was made nicer.

--- graphs.py
+++ graphs.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 import numpy as np
+import math
 
 
 parser = argparse.ArgumentParser(description="Showing results.")
@@ -55,8 +56,21 @@ def read_validation_file(f_name):
 
 
 # Drawing the heat map
-def heat_map(table):
-    fig = table.plot.scatter(x='pn', y='ps', c='error', xlim=(0.0, 1.0), ylim=(0.0, 1.0), marker='s', s=230, cmap='winter')
+def heat_map(f_name):
+
+    table = pd.read_table(f_name, sep=",", names=['ps', 'pn', 'error'], index_col=False)
+    table = table.set_index(['ps', 'pn']).sort_index()
+    length = int(math.sqrt(table.size))
+    Z = np.zeros((length, length))
+
+    for x in range(length):
+        for y in range(length):
+            Z[x, y] = table.iloc[y * length + x]
+
+    fig = plt.imshow(Z, interpolation='bicubic', cmap='winter',
+                    origin='lower', extent=[0, 1, 0, 1],
+                    vmax=abs(Z).max(), vmin=-abs(Z).max())
+
     plt.show(fig)
 
 
@@ -64,10 +78,16 @@ def heat_map(table):
 # ERROR RATES for the heuristic, neural and neural with transfer
 
 def read_errorrate_file(f_name):
-    return pd.read_csv(f_name, sep=',', header=None, names=['length', 'heuristic', 'neural'])
+    return pd.read_csv(f_name, sep=',', header=None, names=['length', 'heuristic', 'neural', 'transfer'])
 
 
 def heur_neur(names):
+    # Agresti-Coull method
+    # cite: Agresti, Alan; Coull, Brent A. (May 1998),
+    # "Approximate is better than 'exact' for interval estimation of binomial proportions",
+    # The American Statistician, 52 (2): 119–126, doi:10.2307/2685469
+
+    z = 1.644  # 90% confidence level
 
     fig, axes = plt.subplots(nrows=2, ncols=2)
     pos = [(0, 0), (0, 1), (1, 0), (1, 1)]
@@ -75,7 +95,19 @@ def heur_neur(names):
         table = (read_errorrate_file(nm)
                  .set_index('length')
                  .sort_index())
-        table.plot(ax=axes[p[0], p[1]])
+        samples = np.array([1000, 500, 200, 200, 200, 100, 100, 100, 100, 100, 100, 50])
+
+        table = table.apply(func=lambda x: (x * samples + 0.5 * z * z) / (samples + z))
+        errors = table.apply(func=lambda x: np.sqrt(x*(1-x)/(samples + z * z)))
+
+        axes[p[0], p[1]].fill_between(errors.index, table['heuristic'] - z*errors['heuristic'],
+                                      table['heuristic'] + z*errors['heuristic'], color='b', alpha='0.2')
+        axes[p[0], p[1]].fill_between(errors.index, table['neural'] - z*errors['neural'],
+                                      table['neural'] + z*errors['neural'], color='g', alpha='0.2')
+        axes[p[0], p[1]].fill_between(errors.index, table['transfer'] - z*errors['transfer'],
+                                      table['transfer'] + z*errors['transfer'], color='r', alpha='0.2')
+
+        table.plot.line(ax=axes[p[0], p[1]])
         axes[p[0], p[1]].set_title(nm)
 
     plt.show(fig)
@@ -83,7 +115,7 @@ def heur_neur(names):
 
 def al_error(names):
 
-    frame = {'Names': names, 'Neural': [], 'Heuristic': []}
+    frame = {'Names': names, 'Neural': [], 'Heuristic': [], 'Transfer': []}
     for nm in names:
         tab = read_errorrate_file(nm)
         tab['heuristic'] = tab['heuristic'].apply(func=lambda x: 1 - x)
@@ -94,12 +126,16 @@ def al_error(names):
         al_neur = (tab['length'] * tab['neural']).sum() / tab['neural'].sum()
         frame['Neural'].append(al_neur)
 
+        tab['transfer'] = tab['transfer'].apply(func=lambda x: 1 - x)
+        al_neur = (tab['length'] * tab['transfer']).sum() / tab['transfer'].sum()
+        frame['Transfer'].append(al_neur)
+
     df = pd.DataFrame(frame).set_index('Names')
 
     return df
 
 
-al_error(['FangShan11.csv', 'HuNan.csv', 'QuYangC.csv', 'YeCheng5.csv'])
+#al_error(['FangShan.csv', 'HuNan.csv', 'QuYangC.csv', 'YeCheng5.csv'])
 
 
 # -------------------------------------------
